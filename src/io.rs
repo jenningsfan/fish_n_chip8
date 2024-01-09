90e9853b5c714395a3e02bb4c842adc8aff82086
Sammy Humphreys <Sammyh2009@outlook.com>
2024-01-09 21:25:10 +0000

    Added load ROM gui menu. However, it draws over part of the CHIP-8 display

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,13 @@
+use ggegui::Gui;
+use ggegui::egui::{self, menu};
+use rfd;
+
 use ggez::{Context, ContextBuilder, GameResult};
 use ggez::audio::{self, SoundSource, Source};
 use ggez::conf::WindowSetup;
 use ggez::event::{self, EventHandler};
 use ggez::glam::Vec2;
-use ggez::graphics::{self, Color, DrawParam, Image, InstanceArray};
+use ggez::graphics::{Canvas, Color, DrawParam, Image, InstanceArray};
 use ggez::input::keyboard::{KeyCode, KeyboardContext, KeyInput};
 
 use std::collections::HashSet;
@@ -18,10 +22,12 @@ pub struct EmulatorIO {
     pixels_batch: InstanceArray,
     beep_sound: Source,
     cpu: CPU,
+    gui: Gui,
+    menu_bar_height: f32, // probs not best practice
 }
 
 impl EmulatorIO {
-    pub fn new(ctx: &mut Context, rom_path: String) -> EmulatorIO {
+    pub fn new(ctx: &mut Context) -> EmulatorIO {
         let pixel_rect = Image::from_color(
             &ctx.gfx,
             PIXEL_SIZE as u32,
@@ -34,36 +40,43 @@ impl EmulatorIO {
             pixels_batch,
             beep_sound: audio::Source::new(ctx, "/beep.wav").unwrap(),
             cpu: CPU::new(),
+            gui: Gui::new(ctx),
+            menu_bar_height: 0.0,
         };
 
         created.beep_sound.set_repeat(true);
 
-        let rom = fs::read(rom_path).unwrap(); // TODO: Error Handling
+        let rom = vec![0x12, 0x00]; // infinte loop
         created.cpu.load_rom(&rom);
 
         created
     }
 
-    fn key_for_keycode(&self, keycode: &KeyCode) -> Option<u8> {
-        match *keycode {
-            KeyCode::Key1 => return Some(0x1),
-            KeyCode::Key2 => return Some(0x2),
-            KeyCode::Key3 => return Some(0x3),
-            KeyCode::Key4 => return Some(0xC),
-            KeyCode::Q => return Some(0x4),
-            KeyCode::W => return Some(0x5),
-            KeyCode::E => return Some(0x6),
-            KeyCode::R => return Some(0xD),
-            KeyCode::A => return Some(0x7),
-            KeyCode::S => return Some(0x8),
-            KeyCode::D => return Some(0x9),
-            KeyCode::F => return Some(0xE),
-            KeyCode::Z => return Some(0xA),
-            KeyCode::X => return Some(0x0),
-            KeyCode::C => return Some(0xB),
-            KeyCode::V => return Some(0xF),
-            _ => return None,
-        };
+    fn key_for_keycode(&self, keycode: Option<&KeyCode>) -> Option<u8> {
+        if let Some(keycode) = keycode {
+            match *keycode {
+                KeyCode::Key1 => return Some(0x1),
+                KeyCode::Key2 => return Some(0x2),
+                KeyCode::Key3 => return Some(0x3),
+                KeyCode::Key4 => return Some(0xC),
+                KeyCode::Q => return Some(0x4),
+                KeyCode::W => return Some(0x5),
+                KeyCode::E => return Some(0x6),
+                KeyCode::R => return Some(0xD),
+                KeyCode::A => return Some(0x7),
+                KeyCode::S => return Some(0x8),
+                KeyCode::D => return Some(0x9),
+                KeyCode::F => return Some(0xE),
+                KeyCode::Z => return Some(0xA),
+                KeyCode::X => return Some(0x0),
+                KeyCode::C => return Some(0xB),
+                KeyCode::V => return Some(0xF),
+                _ => return None,
+            };
+        }
+        else {
+            return None;
+        }
     }
 
     fn get_pressed_keys(&self, key_ctx: &KeyboardContext) -> HashSet<u8> {
@@ -71,27 +84,45 @@ impl EmulatorIO {
         let mut pressed_nums: HashSet<u8> = HashSet::new();
         
         for key in pressed {
-            if let Some(key) = self.key_for_keycode(key) {
+            if let Some(key) = self.key_for_keycode(Some(key)) {
                 pressed_nums.insert(key);
             }
         }
 
         pressed_nums
     }
-}
 
-impl EventHandler for EmulatorIO {
-    fn key_up_event(&mut self, _ctx: &mut Context, input: KeyInput) -> GameResult {
-        let key = self.key_for_keycode(&input.keycode.unwrap());
-
-        if let Some(key) = key  {
-            self.cpu.key_released(key);
-        }
-
-        Ok(())
+    fn draw_gui(&mut self, canvas: &mut Canvas) {
+        canvas.draw(
+            &self.gui, 
+            DrawParam::default().dest(Vec2::ZERO),
+        );
     }
 
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
+    fn draw_pixel_grid(&mut self, _ctx: &mut Context, canvas: &mut Canvas) {
+        // if !self.cpu.pixels_dirty {
+        //     return
+        // }
+        self.pixels_batch.clear();
+
+        for (col_i, row) in self.cpu.pixels.iter().enumerate() {
+            for (row_i, pixel) in row.iter().enumerate() {
+                if *pixel {
+                    self.pixels_batch.push(
+                        DrawParam::new().dest(Vec2::new(
+                            row_i as f32 * PIXEL_SIZE + self.menu_bar_height,
+                            col_i as f32 * PIXEL_SIZE,
+                        )),
+                    );
+                }
+            }
+        }
+
+        self.cpu.pixels_dirty = false;
+        canvas.draw(&self.pixels_batch, DrawParam::new());
+    }
+
+    fn update_cpu(&mut self, ctx: &mut Context) -> GameResult {
         let pressed_keys = self.get_pressed_keys(&ctx.keyboard);
 
         if self.cpu.timer_tick() && !self.beep_sound.playing() {
@@ -103,45 +134,68 @@ impl EventHandler for EmulatorIO {
 
         for _ in 0..12 {
             self.cpu.handle_opcode(&pressed_keys);
-
-            if ctx.time.ticks() % 100 == 0 {
-                println!("Delta frame time: {:?} ", ctx.time.delta());
-                println!("Average FPS: {}", ctx.time.fps());
-            }
         }
 
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        if !self.cpu.pixels_dirty {
-            return Ok(());
-        }
+    fn update_gui(&mut self, ctx: &mut Context) -> GameResult {
+        let gui_ctx = self.gui.ctx();
 
-        let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
-        self.pixels_batch.clear();
-
-        for (col_i, row) in self.cpu.pixels.iter().enumerate() {
-            for (row_i, pixel) in row.iter().enumerate() {
-                if *pixel {
-                    self.pixels_batch.push(
-                        DrawParam::new().dest(Vec2::new(
-                            row_i as f32 * PIXEL_SIZE,
-                            col_i as f32 * PIXEL_SIZE,
-                        )),
-                    );
+        let height = egui::TopBottomPanel::top("MenuBar").show(&gui_ctx, |ui| {
+            menu::bar(ui, |ui| {
+                if ui.button("Load ROM").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_file() {
+                        let rom = fs::read(path).unwrap(); // TODO: Error Handling
+                        self.cpu = CPU::new();
+                        self.cpu.load_rom(&rom);
+                    }
                 }
-            }
+            });
+        }).response.rect.height();
+
+        self.gui.update(ctx);
+        self.menu_bar_height = height;
+        //ctx.gfx.set_drawable_size(SCREEN_SIZE.0, SCREEN_SIZE.1 as f32 + height)?; // make room for whole game
+
+        Ok(())
+    }
+}
+
+impl EventHandler for EmulatorIO {
+    fn key_up_event(&mut self, _ctx: &mut Context, input: KeyInput) -> GameResult {
+        let key = self.key_for_keycode(input.keycode.as_ref());
+
+        if let Some(key) = key  {
+            self.cpu.key_released(key);
         }
 
-        self.cpu.pixels_dirty = false;
+        Ok(())
+    }
 
-        canvas.draw(&self.pixels_batch, DrawParam::new());
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.update_cpu(ctx)?;
+        self.update_gui(ctx)?;
+
+        if ctx.time.ticks() % 100 == 0 {
+            println!("Delta frame time: {:?} ", ctx.time.delta());
+            println!("Average FPS: {}", ctx.time.fps());
+        }
+
+        Ok(())
+    }
+    
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = Canvas::from_frame(ctx, Color::BLACK);
+        
+        self.draw_pixel_grid(ctx, &mut canvas);
+        self.draw_gui(&mut canvas);
+
         canvas.finish(ctx)
     }
 }
 
-pub fn emulator_main(rom_path: String) {
+pub fn emulator_main() {
     let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
         let mut path = path::PathBuf::from(manifest_dir);
         path.push("resources");
@@ -157,7 +211,7 @@ pub fn emulator_main(rom_path: String) {
         .build()
         .expect("Failed to create game context");
 
-    let game = EmulatorIO::new(&mut ctx, rom_path);
+    let game = EmulatorIO::new(&mut ctx);
 
     event::run(ctx, event_loop, game);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::env;
-
 mod cpu;
 mod io;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    io::emulator_main(args[1].clone());
+    io::emulator_main();
 }
